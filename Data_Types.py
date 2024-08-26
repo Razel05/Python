e8d36f403632adb45f39e1d3889676b3c7b83084
Razel05 <zelzilla99@gmail.com>
2024-08-26 15:31:32 -0500

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,21 @@
+age=18
+name = "Razel"
+
+print("My age is",age)
+print("The name is",name)
+
+
+x = 10
+print("literal:",x)
+
+y = -20
+print("integer:", y)
+
+z = 15.5
+print("Float:", z)
+
+name = "Razel"
+print("String:", name)
+
+is_active = True
+print("Boolean", is_active)
