a61a1a9d034f96e1c3c9501664de77b9ca09a6ac
Razel05 <zelzilla99@gmail.com>
2024-09-02 17:41:10 -0500

    Add files via upload

---
+++ better_f_strings.py
@@ -0,0 +1,42 @@
+def kg_to_lbs(kg):
+  """Converts kilograms to pounds."""
+  return kg * 2.20462
+
+# Example usage
+weight_kg = 70
+weight_lbs = kg_to_lbs(weight_kg)
+
+print(f"{weight_kg} kg is equal to {weight_lbs:.2f} lbs")
+
+
+def kg_to_lbs(kg):
+  """Converts kilograms to pounds."""
+  return kg * 2.20462
+
+# Example usage
+weight_kg = 15
+weight_lbs = kg_to_lbs(weight_kg)
+
+print(f"{weight_kg} kg is equal to {weight_lbs:.2f} lbs")
+
+
+def kg_to_lbs(kg):
+  """Converts kilograms to pounds."""
+  return kg * 2.20462
+
+# Example usage
+weight_kg = 63
+weight_lbs = kg_to_lbs(weight_kg)
+
+print(f"{weight_kg} kg is equal to {weight_lbs:.2f} lbs")
+
+def kg_to_lbs(kg):
+  """Converts kilograms to pounds."""
+  return kg * 2.20462
+
+# Example usage
+weight_kg = 3
+weight_lbs = kg_to_lbs(weight_kg)
+
+print(f"{weight_kg} kg is equal to {weight_lbs:.2f} lbs")
+
