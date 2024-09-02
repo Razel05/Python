3ba2b24d81effed721a1aa29e41ccd9184903f5d
Razel05 <zelzilla99@gmail.com>
2024-09-02 18:26:42 -0500

    Add files via upload

---
+++ understanding_comments.py
@@ -0,0 +1,8 @@
+# amount of fuel that fits in the tank
+tank_size = 15
+# How far you're traveling
+distance = 600
+# miles per gallon
+mpg = distance / tank_size
+# This will help you calculate fuel consumption
+print("Miles per gallon:", mpg)
